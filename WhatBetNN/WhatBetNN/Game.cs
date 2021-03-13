11c6945029f9667ddd0c79d2a37b6476e4f27c72
Rinkton <bystrova1979inbox.ru@gmail.com>
2021-03-13 14:54:19 +0300

    Add project

---
+++ WhatBetNN/WhatBetNN/Game.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatBetNN
+{
+    class Game
+    {
+        private NN[] allNNs = new NN[100];
+
+        public Game()
+        {
+            for (int i = 0; i < allNNs.Length; i++)
+            {
+                allNNs[i] = new NN();
+            }
+        }
+
+        public void Main()
+        {
+            int i = 1;
+
+            foreach (NN nn in allNNs)
+            {
+                int money = 10;
+                int howLongLive = 1;
+
+                while (true)
+                {
+                    Console.WriteLine("Your money: " + money);
+
+                    int bet = nn.Think(money);
+                    Console.WriteLine("Your bet: " + bet);
+
+                    if (bet <= 0 || bet > money)
+                    {
+                        Console.WriteLine("You don't have those money!");
+                        continue;
+                    }
+
+                    money -= bet;
+
+                    bool won = new Random().Next(0, 2) == 1;
+
+                    if (won)
+                    {
+                        Console.WriteLine("You won!");
+                        money += bet * 2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You Lose!");
+                    }
+
+                    if (money == 0)
+                    {
+                        Console.WriteLine("You bankrupted");
+                        nn.Lose(howLongLive);
+                        nn.earnedMoney = 0;
+                        i = 0;
+                        break;
+                    }
+
+                    int NNTimes = 1000;
+
+                    if (i > NNTimes)
+                    {
+                        nn.earnedMoney = money;
+                        i = 0;
+                        break;
+                    }
+
+                    nn.CheckProductivity(money, howLongLive);
+                    howLongLive++;
+                    i++;
+                }
+            }
+
+            newNNs();
+        }
+
+        private void newNNs()
+        {
+            List<int> earnedMoney = new List<int>();
+
+            for (int nnNum = 0; nnNum < allNNs.Length; nnNum++)
+            {
+                earnedMoney.Add(allNNs[nnNum].earnedMoney);
+            }
+
+
+            int iter = 0;
+            int bestNumYet = 0;
+            int bestMoneyYet = 0;
+
+            foreach (int earned in earnedMoney)
+            {
+                if (earned > bestMoneyYet)
+                {
+                    bestNumYet = iter;
+                    bestMoneyYet = earned;
+                }
+
+                Console.WriteLine(earned);
+
+                iter++;
+            }
+
+            Console.ReadKey();
+
+            for (int i = 0; i < allNNs.Length; i++)
+            {
+                allNNs[i] = new NN(allNNs[bestNumYet].iow);
+            }
+
+            Main();
+        }
+    }
+}
---
+++ WhatBetNN/WhatBetNN/NN.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatBetNN
+{
+    class NN
+    {
+        public int earnedMoney;
+
+        private readonly int[] inputN = new int[] { 1 };
+        public float iow { get; private set; } // input-output weight
+
+        // Low - Important
+        private const int liveRate = 50;
+        // High - Important
+        private const int productivityRate = 5;
+        private const int unproductivityRate = 3;
+
+        public NN(float iowNum = 5)
+        {
+            iow = iowNum;
+        }
+
+        public int Think(int money)
+        {
+            int betFormule;
+
+            while(true)
+            {
+                betFormule = Convert.ToInt32(inputN[0] * iow);
+
+                if (money == 0)
+                {
+                    return 0;
+                }
+
+                if (money >= betFormule)
+                {
+                    if (betFormule > 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        iow++;
+                    }
+                }
+                else
+                {
+                    iow--;
+                }
+            }
+
+            return betFormule;
+        }
+
+        public void CheckProductivity(int money, int howLongLive)
+        {
+            int unproductivityKoef = (productivityRate * howLongLive) - money;
+
+            if(unproductivityKoef > 0)
+            {
+                iow += (liveRate / howLongLive) * new Random().Next(-2, 2);
+            }
+        }
+
+        public void Lose(int howLongLive)
+        {
+            iow += (liveRate / howLongLive) * new Random().Next(-2, 2);
+        }
+    }
+}
